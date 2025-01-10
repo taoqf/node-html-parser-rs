16cedf2aa67ea79805854d7129f339e489309e53
taoqf <tao_qiufeng@126.com>
2025-01-10 09:04:37 +0800

    ignore warning

--- src/atoms/types.rs
+++ src/atoms/types.rs
@@ -1,5 +1,10 @@
+#[allow(dead_code)]
 pub(crate) type TIMESTAMP = Vec<u8>;
+#[allow(dead_code)]
 pub(crate) type DATETIME = chrono::NaiveDateTime;
+#[allow(dead_code)]
 pub(crate) type DECIMAL = f64;
+#[allow(dead_code)]
 pub(crate) type DECIMALL = f64;
+#[allow(dead_code)]
 pub(crate) type FLOAT = f64;
