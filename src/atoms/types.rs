e80724ae834f244c1f1cc8c799fba3f1e79f8904
taoqf <tao_qiufeng@126.com>
2025-01-11 11:59:46 +0800

    fix: types

--- src/atoms/types.rs
+++ src/atoms/types.rs
@@ -3,7 +3,7 @@ pub(crate) type TIMESTAMP = Vec<u8>;
 #[allow(dead_code)]
 pub(crate) type DATETIME = chrono::NaiveDateTime;
 #[allow(dead_code)]
-pub(crate) type DECIMAL = f64;
+pub(crate) type DECIMAL = tiberius::numeric::Decimal;
 #[allow(dead_code)]
 pub(crate) type DECIMALL = tiberius::numeric::Decimal;
 #[allow(dead_code)]
