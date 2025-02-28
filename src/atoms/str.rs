e9401f648b069dea1acdd57efd2ccf07f67196f5
taoqf <tao_qiufeng@126.com>
2025-02-28 17:01:22 +0800

    add atom

--- src/atoms/str.rs
+++ src/atoms/str.rs
@@ -5,3 +5,11 @@ pub(crate) fn is_none_or_empty(s: Option<&str>) -> bool {
 		None => true,
 	}
 }
+
+#[allow(dead_code)]
+pub(crate) fn get_opt_str(val: &Option<String>) -> &str {
+	match val {
+		Some(val) => val.as_str(),
+		None => "",
+	}
+}
