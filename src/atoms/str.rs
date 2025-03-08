7d2a10bab235445f67bf1c0d91dda46684af2f09
taoqf <tao_qiufeng@126.com>
2025-03-08 14:25:30 +0800

    add atom

--- src/atoms/str.rs
+++ src/atoms/str.rs
@@ -7,9 +7,14 @@ pub(crate) fn is_none_or_empty(s: Option<&str>) -> bool {
 }
 
 #[allow(dead_code)]
-pub(crate) fn get_opt_str(val: &Option<String>) -> &str {
+pub(crate) fn opt2str(val: &Option<String>) -> &str {
 	match val {
 		Some(val) => val.as_str(),
 		None => "",
 	}
+}
+
+#[allow(dead_code)]
+pub(crate) fn str2opt(val: &str) -> Option<String> {
+	return Some(val.to_owned());
 }
