d9f90b48854112b62696a900d3c1cef1dfebffdf
taoqf <tao_qiufeng@126.com>
2025-03-18 15:18:23 +0800

    add atom smartsheet manage fields

--- src/atoms/weixin/work/doc/smartsheet.rs
+++ src/atoms/weixin/work/doc/smartsheet.rs
@@ -171,7 +171,7 @@ impl super::super::index::WeixinWork {
 		&mut self,
 		docid: &str,
 		sheet_id: &str,
-		records: Vec<serde_json::Value>,
+		records: &Vec<serde_json::Value>,
 	) {
 		assert!(docid.is_empty() == false, "doc_id could not be empty");
 		assert!(sheet_id.is_empty() == false, "sheet_id could not be empty");
@@ -180,7 +180,7 @@ impl super::super::index::WeixinWork {
 			"https://qyapi.weixin.qq.com/cgi-bin/wedoc/smartsheet/update_records?access_token={token}"
 		);
 		#[derive(Debug, serde:: Deserialize)]
-		struct AddSheetRecordResult {
+		struct UpdateSheetRecordResult {
 			errcode: i32,
 			errmsg: String,
 			records: serde_json::Value,
@@ -197,7 +197,7 @@ impl super::super::index::WeixinWork {
 			.send()
 			.await
 			.unwrap()
-			.json::<AddSheetRecordResult>()
+			.json::<UpdateSheetRecordResult>()
 			.await
 			.unwrap();
 		log::debug!("update records result: {:?}", ret);
@@ -243,5 +243,335 @@ impl super::super::index::WeixinWork {
 			.unwrap();
 		log::debug!("del sheet records result: {:?}", ret);
 		assert!(ret.errcode == 0, "failed to delete records: {}", ret.errmsg);
+	}
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct NumberFieldProperty {
+	pub(crate) decimal_places: i32,
+	pub(crate) use_separate: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct CheckboxFieldProperty {
+	pub(crate) checked: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct DateTimeFieldProperty {
+	pub(crate) format: String,
+	pub(crate) auto_fill: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct AttachmentFieldProperty {
+	pub(crate) display_mode: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct UserFieldProperty {
+	pub(crate) is_multiple: bool,
+	pub(crate) is_notified: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct UrlFieldProperty {
+	#[serde(rename = "type")]
+	pub(crate) url_type: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct SelectOption {
+	pub(crate) id: String,
+	pub(crate) text: String,
+	pub(crate) style: i32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct SelectFieldProperty {
+	pub(crate) is_quick_add: bool,
+	pub(crate) options: Vec<SelectOption>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct CreatedTimeFieldProperty {
+	pub(crate) format: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct ModifiedTimeFieldProperty {
+	pub(crate) format: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct ProgressFieldProperty {
+	pub(crate) decimal_places: i32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct SingleSelectFieldProperty {
+	pub(crate) is_quick_add: bool,
+	pub(crate) options: Vec<SelectOption>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct ReferenceFieldProperty {
+	/// 关联的子表id，为空时，表示关联本子表
+	pub(crate) sub_id: Option<String>,
+	pub(crate) filed_id: String,
+	pub(crate) is_multiple: bool,
+	pub(crate) view_id: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct LocationFieldProperty {
+	pub(crate) input_type: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct NumberRule {
+	#[serde(rename = "type")]
+	pub(crate) rule_type: String,
+	pub(crate) value: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct AutoNumberFieldProperty {
+	#[serde(rename = "type")]
+	pub(crate) auto_type: String,
+	pub(crate) rules: Vec<NumberRule>,
+	pub(crate) reformat_existing_record: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct CurrencyFieldProperty {
+	pub(crate) currency_type: String,
+	pub(crate) decimal_places: i32,
+	pub(crate) use_separate: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct WwGroupFieldProperty {
+	pub(crate) allow_multiple: bool,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, serde::Serialize, serde:: Deserialize)]
+pub(crate) struct Field {
+	pub(crate) field_id: String,
+	pub(crate) field_title: String,
+	pub(crate) field_type: String,
+	pub(crate) property_number: Option<NumberFieldProperty>,
+	pub(crate) property_checkbox: Option<CheckboxFieldProperty>,
+	pub(crate) property_date_time: Option<DateTimeFieldProperty>,
+	pub(crate) property_attachment: Option<AttachmentFieldProperty>,
+	pub(crate) property_user: Option<UserFieldProperty>,
+	pub(crate) property_url: Option<UrlFieldProperty>,
+	pub(crate) property_select: Option<SelectFieldProperty>,
+	pub(crate) property_created_time: Option<CreatedTimeFieldProperty>,
+	pub(crate) property_modified_time: Option<ModifiedTimeFieldProperty>,
+	pub(crate) property_progress: Option<ProgressFieldProperty>,
+	pub(crate) property_single_select: Option<SingleSelectFieldProperty>,
+	pub(crate) property_reference: Option<ReferenceFieldProperty>,
+	pub(crate) property_location: Option<LocationFieldProperty>,
+	pub(crate) property_auto_number: Option<AutoNumberFieldProperty>,
+	pub(crate) property_currency: Option<CurrencyFieldProperty>,
+	pub(crate) property_ww_group: Option<WwGroupFieldProperty>,
+}
+
+#[allow(dead_code)]
+impl super::super::index::WeixinWork {
+	/// 查询字段
+	/// @see https://developer.work.weixin.qq.com/document/path/99914
+	pub(crate) async fn doc_smartsheet_fields_get(
+		&mut self,
+		docid: &str,
+		sheet_id: &str,
+	) -> Vec<Field> {
+		assert!(docid.is_empty() == false, "doc_id could not be empty");
+		assert!(sheet_id.is_empty() == false, "sheet_id could not be empty");
+		let token = self.get_access_token().await;
+		let url = format!(
+			"https://qyapi.weixin.qq.com/cgi-bin/wedoc/smartsheet/get_fields?access_token={token}"
+		);
+		#[derive(Debug, serde:: Deserialize)]
+		struct SheetGetFieldsResult {
+			errcode: i32,
+			errmsg: String,
+			total: i32,
+			fields: Vec<Field>,
+		}
+		let client = reqwest::Client::new();
+		let ret = client
+			.post(url)
+			.json(&serde_json::json!({
+				"docid":docid,
+				"sheet_id":sheet_id,
+			}))
+			.send()
+			.await
+			.unwrap()
+			.json::<SheetGetFieldsResult>()
+			.await
+			.unwrap();
+		log::debug!("get sheet fields result: {:?}", ret);
+		assert!(
+			ret.errcode == 0,
+			"failed to get sheet fields: {}",
+			ret.errmsg
+		);
+		return ret.fields;
+	}
+}
+
+#[allow(dead_code)]
+impl super::super::index::WeixinWork {
+	/// 添加字段
+	/// @see https://developer.work.weixin.qq.com/document/path/99904
+	pub(crate) async fn doc_smartsheet_fields_add(
+		&mut self,
+		docid: &str,
+		sheet_id: &str,
+		fields: &Vec<Field>,
+	) {
+		assert!(docid.is_empty() == false, "doc_id could not be empty");
+		assert!(sheet_id.is_empty() == false, "sheet_id could not be empty");
+		let token = self.get_access_token().await;
+		let url = format!(
+			"https://qyapi.weixin.qq.com/cgi-bin/wedoc/smartsheet/add_fields?access_token={token}"
+		);
+		#[derive(Debug, serde:: Deserialize)]
+		struct AddFieldsResult {
+			errcode: i32,
+			errmsg: String,
+			fields: Vec<Field>,
+		}
+		let client = reqwest::Client::new();
+		let ret = client
+			.post(url)
+			.json(&serde_json::json!({
+				"docid":docid,
+				"sheet_id":sheet_id,
+				"fields":fields,
+			}))
+			.send()
+			.await
+			.unwrap()
+			.json::<AddFieldsResult>()
+			.await
+			.unwrap();
+		log::debug!("add sheet fields result: {:?}", ret);
+		assert!(
+			ret.errcode == 0,
+			"failed to add sheet fields: {}",
+			ret.errmsg
+		);
+	}
+}
+
+#[allow(dead_code)]
+impl super::super::index::WeixinWork {
+	/// 更新字段
+	/// @see https://developer.work.weixin.qq.com/document/path/99906
+	pub(crate) async fn doc_smartsheet_fields_update(
+		&mut self,
+		docid: &str,
+		sheet_id: &str,
+		fields: &Vec<Field>,
+	) {
+		assert!(docid.is_empty() == false, "doc_id could not be empty");
+		assert!(sheet_id.is_empty() == false, "sheet_id could not be empty");
+		let token = self.get_access_token().await;
+		let url = format!(
+			"https://qyapi.weixin.qq.com/cgi-bin/wedoc/smartsheet/update_fields?access_token={token}"
+		);
+		#[derive(Debug, serde:: Deserialize)]
+		struct UpdateFieldsResult {
+			errcode: i32,
+			errmsg: String,
+			fields: Vec<Field>,
+		}
+		let client = reqwest::Client::new();
+		let ret = client
+			.post(url)
+			.json(&serde_json::json!({
+				"docid":docid,
+				"sheet_id":sheet_id,
+				"fields":fields,
+			}))
+			.send()
+			.await
+			.unwrap()
+			.json::<UpdateFieldsResult>()
+			.await
+			.unwrap();
+		log::debug!("update sheet fields result: {:?}", ret);
+		assert!(
+			ret.errcode == 0,
+			"failed to update sheet fields: {}",
+			ret.errmsg
+		);
+	}
+}
+
+#[allow(dead_code)]
+impl super::super::index::WeixinWork {
+	/// 删除字段
+	/// @see https://developer.work.weixin.qq.com/document/path/99905
+	pub(crate) async fn doc_smartsheet_fields_del(
+		&mut self,
+		docid: &str,
+		sheet_id: &str,
+		field_ids: &Vec<String>,
+	) {
+		assert!(docid.is_empty() == false, "doc_id could not be empty");
+		assert!(sheet_id.is_empty() == false, "sheet_id could not be empty");
+		let token = self.get_access_token().await;
+		let url = format!(
+			"https://qyapi.weixin.qq.com/cgi-bin/wedoc/smartsheet/update_fields?access_token={token}"
+		);
+		#[derive(Debug, serde:: Deserialize)]
+		struct DeleteFieldsResult {
+			errcode: i32,
+			errmsg: String,
+		}
+		let client = reqwest::Client::new();
+		let ret = client
+			.post(url)
+			.json(&serde_json::json!({
+				"docid":docid,
+				"sheet_id":sheet_id,
+				"field_ids":field_ids,
+			}))
+			.send()
+			.await
+			.unwrap()
+			.json::<DeleteFieldsResult>()
+			.await
+			.unwrap();
+		log::debug!("update sheet fields result: {:?}", ret);
+		assert!(
+			ret.errcode == 0,
+			"failed to update sheet fields: {}",
+			ret.errmsg
+		);
 	}
 }
