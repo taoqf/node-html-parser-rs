01cce0d8d77809d7820799f8a692471af1aad27f
taoqf <tao_qiufeng@126.com>
2025-03-19 18:17:06 +0800

    add method

--- src/atoms/doccode.rs
+++ src/atoms/doccode.rs
@@ -1,3 +1,5 @@
+use chrono::{Datelike, Timelike};
+
 const URI: &str = "http://doccode:8890";
 
 #[allow(dead_code)]
@@ -20,4 +22,38 @@ pub(crate) async fn doccode(name: &str, num: usize, len: usize) -> Vec<String> {
 		.await
 		.unwrap();
 	return data;
+}
+
+#[allow(dead_code)]
+pub(crate) async fn doccode_with_dt(name: &str, num: usize, len: usize) -> Vec<String> {
+	if len == 0 {
+		return vec![];
+	}
+	let now = crate::atoms::dt::now();
+	let year = now.year();
+	let month = now.month();
+	let day = now.day();
+	let name = format!("{name}{:04}{:02}{:02}", year, month, day);
+
+	return doccode(&name, num, len).await;
+}
+
+#[allow(dead_code)]
+pub(crate) async fn doccode_with_tm(name: &str, num: usize, len: usize) -> Vec<String> {
+	if len == 0 {
+		return vec![];
+	}
+	let now = crate::atoms::dt::now();
+	let year = now.year();
+	let month = now.month();
+	let day = now.day();
+	let hour = now.hour();
+	let minute = now.minute();
+	let second = now.second();
+	let name = format!(
+		"{name}{:04}{:02}{:02}{:02}{:02}{:02}",
+		year, month, day, hour, minute, second
+	);
+
+	return doccode(&name, num, len).await;
 }
