f7a2879a85f12de5fad47d34ce962b9841b3ead3
taoqf <tao_qiufeng@126.com>
2025-04-02 09:00:24 +0800

    appid

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ pub(crate) use app_state::get_state;
 async fn main() {
 	log4rs::init_file("./log4rs.yaml", Default::default()).expect("log4rs config not correct!");
 	dotenvy::dotenv().unwrap();
+	let state = crate::get_state().await;
 	actix_web::HttpServer::new(move || {
 		actix_web::App::new().service(
-			actix_web::web::scope("/api")
+			actix_web::web::scope(&state.appid)
 				.service(api::demo::hello)
 				.service(api::demo::test_post),
 		)
