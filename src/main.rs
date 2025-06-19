ebd6c212581015e6284d870f95a872ab7125f646
taoqf <tao_qiufeng@126.com>
2025-06-19 14:37:55 +0800

    auth

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ async fn main() {
 	actix_web::HttpServer::new(move || {
 		actix_web::App::new()
 			.wrap(actix_web::middleware::Logger::default())
+			.wrap(actix_web::middleware::from_fn(auth_middleware))
 			.service(
 				actix_web::web::scope(&state.appid)
 					.service(actix_web::web::scope("g000").service(api::g000::s000::a001)),
@@ -29,4 +30,36 @@ async fn main() {
 	.run()
 	.await
 	.unwrap();
+}
+
+async fn auth_middleware(
+	req: actix_web::dev::ServiceRequest,
+	next: actix_web::middleware::Next<impl actix_web::body::MessageBody>,
+) -> Result<actix_web::dev::ServiceResponse<impl actix_web::body::MessageBody>, actix_web::Error> {
+	let uri = req.uri().to_string();
+	log::debug!("Calling {}", uri);
+	let headers = req.headers();
+	// 用户鉴权
+	if let Some(auth_header) = headers.get("Authorization") {
+		// 模拟异步用户鉴权
+		if auth_header == "valid-token" {
+			// 如果鉴权成功，继续处理请求
+			let res = next.call(req).await;
+			return match res {
+				Ok(res) => {
+					log::debug!("Success calling {}", uri);
+					Ok(res)
+				}
+				Err(err) => {
+					// 调用服务失败
+					log::error!("Runtime Error while calling {}:{}", uri, err);
+					Err(err)
+				}
+			};
+		}
+	}
+
+	return Err(actix_web::Error::from(err::AppError::from(
+		anyhow::Error::msg("Unauthorized"),
+	)));
 }
